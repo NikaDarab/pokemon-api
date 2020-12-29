f181dd05c907de8b100f12f12252217e42100209
Nika Darab <nikadarab@Nikas-Air.attlocal.net>
2020-12-29 14:52:26 -0600

    app is litening on port 8000 and is working

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
-console.log('hello')
+//imports
+const express = require("express");
+const morgan = require("morgan");
+
+//make an instance of the express
+const app = express();
+
+//using the morgan middleware
+app.use(morgan("dev"));
+
+//post a message
+app.use((req, res) => {
+  res.send("Pokemon go!");
+});
+const PORT = 8000;
+//listening to the port
+app.listen(PORT, () => {
+  console.log(`server is listening at http://localhost:${PORT}`);
+});
